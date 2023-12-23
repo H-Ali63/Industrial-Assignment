802754ea1c33126faeb8726a10d2540ef3fef524
Md Haidar Ali <151657596+H-Ali63@users.noreply.github.com>
2023-12-23 19:00:35 +0530

    First Assignment is uploaded successfully
    No caption

---
+++ First_Assignment01.java
@@ -0,0 +1,38 @@
+package Assingment;
+public class First_Assignment01 {
+
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int current = 0;
+
+        while (current <= high) {
+            if (nums[current] == 0) {
+                swap(nums, low, current);
+                low++;
+                current++;
+            } else if (nums[current] == 2) {
+                swap(nums, current, high);
+                high--;
+            } else {
+                current++;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        sortColors(nums);
+
+        System.out.print("Sorted Colors: ");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
